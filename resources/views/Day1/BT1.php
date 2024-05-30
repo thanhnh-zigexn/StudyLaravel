fa8bcc27388cf66679d07e5e3e2ba10eb526da73
Nguyen Ha Thanh <thanhnh@zigexn-v126s-MacBook-Pro.local>
2024-05-30 17:40:44 +0700

    Day1_BT1

---
+++ resources/views/Day1/BT1.php
@@ -0,0 +1,31 @@
+<?php					
+$a = array(array(1,2,3),array(4,5,6));					
+$b = array('duy','linh');					
+$c = array();					
+					
+for($i = 0 ; $i < count($a); $i ++ )					
+{					
+$item = $a[$i];					
+$item[] = $b[$i];					
+$c[] = $item;					
+}					
+					
+function printArray($array) {					
+	foreach($array as $item) {				
+	echo $item;				
+}					
+}					
+					
+foreach($c as $c_item) {					
+	printArray($c_item);				
+echo "<br/>";					
+					
+}					
+					
+					
+?>					
+					
+					
+					
+					
+					
